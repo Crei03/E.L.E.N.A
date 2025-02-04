7ad0cee4f020891233374efcf79b4feef79cba7f
crei03 <carlosreina2100@gmail.com>
2025-02-04 11:14:01 -0500

    chat disponible

--- server/index.js
+++ server/index.js
@@ -3,32 +3,67 @@ import logger from "morgan";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import generateText from "./gemini.cjs";
+import dotenv from "dotenv";
+import { createClient } from "@libsql/client";
 
+dotenv.config();
 const port = process.env.PORT ?? 3000;
 
 const app = express();
 const server = createServer(app);
 
-const io = new Server(server);
+const io = new Server(server,{
+  connectionStateRecovery: {}
+});
+
+const db = createClient({
+  url: process.env.url,
+  authToken: process.env.authToken,
+})
+
 
 io.on("connection", (socket) => {
   console.log("A user connected");
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
+  // Recover historical messages only once per connection.
+  if (!socket.recovered) {
+    (async () => {
+      try {
+        const username = socket.handshake.auth.username ;
+        const results = await db.execute({
+          sql: 'SELECT id, content, user FROM messages WHERE id > ?',
+          args: [socket.handshake.auth.serverOffset ?? 0]
+        });
+        results.rows.forEach(row => {
+          socket.emit('chat message', row.content, row.id.toString(), row.user);
+        });
+      } catch (e) {
+        console.error(e);
+      }
+      socket.recovered = true;
+    })();
+  }
+
+  socket.on('chat message', async (msg) => {
+    let result;
+    const username = socket.handshake.auth.username ?? 'Anonimo';
+    console.log({ username });
+    try {
+      result = await db.execute({
+        sql: `INSERT INTO messages (content, user) VALUES (:msg, :username)`,
+        args: { msg, username }
+      });
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+    
+    // Emit the new message to all sockets.
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username);
   });
 
-  socket.on("chat message", (msg) => {
-    socket.broadcast.emit("chat message", msg);
-
-    // generateText(msg)
-    //   .then((response) => {
-    //     io.emit("chat message", response);
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
 
